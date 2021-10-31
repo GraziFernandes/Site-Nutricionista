257e141b429c7527a5d19d49c578937a8274105f
Grazielly Fernandes Carneiro Costa <grazielly.fernandescarneiro@hotmail.com>
2021-10-31 14:25:52 -0300

    new commit

---
+++ js/remover-pacientes.js
@@ -0,0 +1,13 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
+
+});
+console.log(pacientes);
